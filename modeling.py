1c54fad639f2a8da45c6662f94789fb580372b71
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2022-10-12 00:27:00 +0100

    Random Forests modeling

--- modeling.py
+++ modeling.py
@@ -15,13 +15,13 @@ import pickle
 #save models and transformers
 def save_estimator(file_name,model):
     # create an iterator object with write permission - model.pkl
-    with open(f'{file_name}', 'wb') as files:
+    with open(f'Models/{file_name}', 'wb') as files:
         pickle.dump(model, files)
         
 #load models and transformers
 def load_estimator(file_name):
     # load saved model
-    with open(f'{file_name}' , 'rb') as f:
+    with open(f'Models/{file_name}' , 'rb') as f:
         model = pickle.load(f)
     return model
 
