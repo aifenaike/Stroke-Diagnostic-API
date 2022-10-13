8a7bb8d51fab00ee4f4a2fcb8175d736bf4788b6
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2022-10-14 00:18:13 +0100

    Utility helper functions

--- utils.py
+++ utils.py
@@ -12,3 +12,10 @@ def load_estimator(file_name):
     with open(f'Models/{file_name}' , 'rb') as f:
         model = pickle.load(f)
     return model
+
+def correct(df):
+    df.gender = df.gender.str.title()
+    df.ever_married = df.ever_married.str.title()
+    df.Residence_type = df.Residence_type.str.title()
+    return df
+
