d834cf1fd45ec8e7b7fc170698e68ab315759248
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2022-10-17 01:58:12 +0100

    input validation script
    This script validates the datatype of every input that the API recieves

--- input_validation.py
+++ input_validation.py
@@ -3,13 +3,14 @@ from enum import Enum
 from pydantic import BaseModel
 from typing import Annotated
 
-
+#specify custom datatype for smoking_status
 class SmokingType(str,Enum):
     formerly_smoked = 'formerly smoked'
     never_smoked = 'never smoked' 
     smokes = 'smokes'
     Unknown = 'Unknown'
 
+#specify custom datatype for work_status
 class WorkType(str, Enum):
     Private = 'Private'
     self_employed = 'Self-employed'
@@ -17,11 +18,19 @@ class WorkType(str, Enum):
     children = 'children'
     Never_worked = 'Never_worked'
 
+#specify custom datatype for gender
+class GenderType(str, Enum):
+    Male= "Male"
+    Female ="Female"
+    Other = "Other"
+
 @dataclass
 class ValueRange:
     min: int
     max: int
 
+
+#===========To abstract dataframe in app.py=================
 # class input_data(BaseModel):
 #     gender:str,
 #     age:int,
@@ -33,4 +42,7 @@ class ValueRange:
 #     avg_glucose_level:float,
 #     bmi: float,
 #     smoking_status:SmokingType,
+
+# data:input_data
+# data_dict = data.dict
 
